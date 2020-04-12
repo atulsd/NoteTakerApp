a389cf70e10c53e9afadff211ae1b976d8108a9c
Atul Mahajan <luta_atul@hotmail.com>
2020-04-12 18:28:08 +1000

    If db.json is empty then generating new id and then adding data to the file.

--- Develop/server.js
+++ Develop/server.js
@@ -17,11 +17,6 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var notes = [
-  { id: 1, title: "Test Title", text: "Test text" },
-  { id: 2, title: "Test Title", text: "Test text" },
-];
-let newNote;
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
@@ -40,29 +35,70 @@ app.get("/assets/js/index.js", function (req, res) {
 });
 
 app.get("/api/notes", function (req, res) {
-  return res.json(notes);
+  readFileAsync("./db/db.json", "utf8").then(function (data) {
+    if (data) {
+      const noteJSON = JSON.parse(data);
+      return res.json(noteJSON);
+    } else {
+      return res.json("");
+    }
+  });
+});
+
+// Create New Notes - takes in JSON input
+app.post("/api/notes", function (req, res) {
+  const writeNotes = [];
+  let newNote;
+  readFileAsync("./db/db.json", "utf8").then(function (data) {
+    // Checking if db.json contains any data or empty.
+    if (data) {
+      const noteJSON = JSON.parse(data);
+      //Getting id of the last note in db.json
+      let lastId = noteJSON[noteJSON.length - 1].id;
+      newNote = req.body;
+      newNote.id = ++lastId;
+      // For each note in note taker
+      noteJSON.forEach(function (note) {
+        writeNotes.push(note);
+      });
+    } else {
+      newNote = req.body;
+      //Initialising id with 1 when first note is being added into the file.
+      newNote.id = 1;
+    }
+
+    writeNotes.push(newNote);
+    const addNoteJSON = JSON.stringify(writeNotes, null, 2);
+    writeFileAsync("db/db.json", addNoteJSON);
+    res.json(newNote);
+  });
 });
 
 app.delete("/api/notes/:id", function (req, res) {
   var selected = req.params.id;
-  console.log("Server Id is: ", selected);
-  for (var i = 0; i < notes.length; i++) {
-    if (parseInt(selected) === notes[i].id) {
-      console.log("second if: ", i);
-      notes.splice(notes[i], 1);
-      return res.json(notes);
+  readFileAsync("./db/db.json", "utf8").then(function (data) {
+    // Parse the JSON string to an object
+    const notesJSON = JSON.parse(data);
+    const notes = [];
+    // For each element in notes
+    notesJSON.forEach(function (note) {
+      if (!(note.id === parseInt(selected))) {
+        notes.push(note);
+      }
+    });
+    // Turn the arrays into JSON strings so they can be written to files
+    const writeJSON = JSON.stringify(notes, null, 2);
+    //Checking db.Json is empty with "[]" or containing any data.
+    if (writeJSON.length === 2) {
+      writeFileAsync("./db/db.json", "").then(function () {
+        return res.json(notes);
+      });
+    } else {
+      writeFileAsync("./db/db.json", writeJSON).then(function () {
+        return res.json(notes);
+      });
     }
-  }
-  // return res.json(false);
-});
-// Create New Notes - takes in JSON input
-app.post("/api/notes", function (req, res) {
-  newNote = req.body;
-  console.log("Req. body is:", req.body);
-  newNote.id = ++notes.length;
-  notes.push(newNote);
-  console.log("Total is:", notes);
-  res.json(newNote);
+  });
 });
 
 // Starts the server to begin listening
