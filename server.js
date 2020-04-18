ad80367a06af0588ab0bce0dc21a938e88d5de2a
Atul Mahajan <luta_atul@hotmail.com>
2020-04-18 14:43:26 +1000

    Changing port to PORT.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 var express = require("express");
 var app = express();
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
